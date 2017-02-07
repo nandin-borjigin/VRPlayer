6e5d2de812c6e59e705d0e72c675ecd1af8f0411
Nandiin <nandiin@163.com>
2017-02-07 16:36:07 +0800

    Created new project: Attitude Estimator

---
+++ lib/AttitudeEstimator/AttitudeEstimator/AttitudeEstimator.h
@@ -0,0 +1,19 @@
+//
+//  AttitudeEstimator.h
+//  AttitudeEstimator
+//
+//  Created by admin on 07/02/2017.
+//  Copyright © 2017 Action VR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AttitudeEstimator.
+FOUNDATION_EXPORT double AttitudeEstimatorVersionNumber;
+
+//! Project version string for AttitudeEstimator.
+FOUNDATION_EXPORT const unsigned char AttitudeEstimatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AttitudeEstimator/PublicHeader.h>
+
+
