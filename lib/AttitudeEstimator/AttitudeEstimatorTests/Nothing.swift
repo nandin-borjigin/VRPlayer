114522aaf85a11a44a1cf4d169c2ba41886be68e
Nandiin <nandiin@163.com>
2017-02-07 16:43:30 +0800

    First failing test to make sure Quick & Nimble is working

---
+++ lib/AttitudeEstimator/AttitudeEstimatorTests/Nothing.swift
@@ -0,0 +1,20 @@
+//
+//  Nothing.swift
+//  AttitudeEstimator
+//
+//  Created by admin on 07/02/2017.
+//  Copyright © 2017 Action VR. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class NothingSpec: QuickSpec {
+    override func spec() {
+        describe("failing test") {
+            it("should fail") {
+                expect(false).to(beTrue())
+            }
+        }
+    }
+}
